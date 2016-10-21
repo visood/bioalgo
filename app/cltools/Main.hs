227e2820d059c09029ec1b42d6b1204587ffaec4
Vishal Sood <vishal.chandra.sood@gmail.com>
2016-10-21 16:40:35 +0200

    define/use data-type Command to parse cli

---
+++ app/cltools/Main.hs
@@ -0,0 +1,29 @@
+module Main (main) where
+
+import Control.Monad
+import System.Environment(getArgs)
+import System.Exit
+import Data.Char
+import Data.Map (Map, (!))
+import qualified Data.Map as M
+import Lib
+import Kmers
+
+main :: IO ()
+main = do
+  args <- getArgs
+  putStr "Command: "
+  print args
+  dispatch args
+
+dispatch        :: [String] -> IO ()
+dispatch ["-h"] = usage >> exit
+dispatch ["-v"] = version >> exit
+dispatch []     = getContents >> exit
+dispatch args   = execute (readCommand args)
+
+usage          = putStrLn "Usage: [-vh] [-k=<kmer-size> -n=<top-kmers> -f=<sequence-file]"
+version        = putStrLn "version 0.1"
+exit           = exitWith ExitSuccess
+die            = exitWith (ExitFailure 1)
+
--- src/lib/Lib.hs
+++ src/lib/Lib.hs
@@ -1,6 +1,60 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Map (Map, (!))
+import qualified Data.Map as M
+import Kmers
+
+data Command = Command {utility  :: String,
+                        arguments :: Map String String
+                       } deriving (Show)
+
+emptyCommand :: Command
+emptyCommand = Command "do-nothing" M.empty
+
+readCommand :: [String] -> Command
+readCommand [] = emptyCommand
+readCommand (u:args) = Command u (readArgs args)
+
+readArgs :: [String] -> Map String String
+readArgs = M.fromList . (map readOneArg)
+
+readOneArg :: String -> (String, String)
+readOneArg s = (dropWhile (=='-') u, dropWhile (=='=') v)
+  where (u, v) = break (=='=') s
+
+
+execute :: Command -> IO ()
+
+execute (Command "hello" _) = do
+  putStrLn "Hello jee! What may bioalgo tools do for you?"
+  putStrLn "Please call me with a command"
+  putStrLn "!!! BYE for now !!!"
+
+
+execute (Command "most-frequent-kmers" argMap) = do
+  text <- readFile f
+  kcounts <- return $ kmerCounts text k
+  putStr (show (sum kcounts))
+  putStr (" total ")
+  putStr (show k)
+  putStrLn "-mers found."
+  putStr " of which "
+  putStr (show (length kcounts))
+  putStrLn " are unique."
+  putStr "Top "
+  putStr (show n)
+  putStr (" ")
+  putStr (show k)
+  putStrLn "-mers: "
+  mapM_ (\(s, n) -> putStrLn ( (show n) ++ ": " ++ s)) (
+    topFrequentKmers n kcounts
+    )
+  where
+    k = read (argMap ! "k") :: Int
+    n = read (argMap ! "n") :: Int
+    f = argMap ! "f"
+
+execute (Command s _) = do
+  putStr "unknown command "
+  putStrLn s
+
