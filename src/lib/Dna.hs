116196e766c0da397128b663fe28157d08fd848a
Vishal Sood <vishal.chandra.sood@gmail.com>
2016-11-03 12:09:41 +0100

    use QuickCkeck and start the test-suite

--- src/lib/Dna.hs
+++ src/lib/Dna.hs
@@ -9,14 +9,20 @@ invalidPos :: Int
 invalidPos = -1
 
 class Ord a => Base a where
-  allowed     :: [a]
+  bases       :: [a]
+  indexed     :: Int -> a --to use in random generation
   complement  :: a -> a
   valid       :: a -> Bool
-  valid x     = elem x allowed
+  valid x     = elem x bases
   symbol      :: a -> Char
 
 instance Base Char where
-  allowed      = ['A', 'C', 'G', 'T', 'N', 'a', 'c', 'g', 't']
+  bases        = ['A', 'C', 'G', 'T', 'N', 'a', 'c', 'g', 't']
+  indexed 0    = 'A'
+  indexed 1    = 'C'
+  indexed 2    = 'G'
+  indexed 3    = 'T'
+  indexed _    = 'N'
   complement c = case c of
     'A' -> 'T'
     'a' -> 't'
@@ -32,6 +38,21 @@ instance Base Char where
 
   symbol c = c
 
+instance Base Int where
+  bases        = [0, 1, 2, 3, 4]
+  indexed i    = if (i < 0) then 4 else min i 4
+  complement c = case c of
+   0 -> 3
+   1 -> 2
+   2 -> 1
+   3 -> 0
+   4 -> 4
+   _ -> c
+  symbol c     = case c of
+    0 -> 'A'
+    1 -> 'C'
+    2 -> 'G'
+    3 -> 'T'
 
 data Sense = Reverse | Forward
              deriving (Eq, Ord, Show, Read)
@@ -41,7 +62,6 @@ anti Reverse = Forward
 anti Forward = Reverse
 
 reverseComplement :: Base a => [a] -> [a]
-
 reverseComplement = foldl (\rs x -> (complement x):rs) []
 
 isBase :: Char -> Bool
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,24 @@
+import Dna
+
+import Test.QuickCheck
+import Test.HUnit
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  putStrLn " tests"
+  putStrLn "test that Char Base can be faithfully complemented"
+  quickCheck (prop_idempotent_complement :: Char -> Bool)
+  putStrLn "test that Int Base can be faithfully complemented"
+  quickCheck (prop_idempotent_complement :: Int -> Bool)
+  putStrLn "test that a sequence of Base Char can be faithfully reverse complemented"
+  quickCheck (prop_idempotent_revcomp :: [Char] -> Bool)
+  putStrLn "test that a sequence of Base Int can be faithfully reverse complemented"
+  quickCheck (prop_idempotent_revcomp :: [Int] -> Bool)
+
+
+
+prop_idempotent_complement :: Base a => a -> Bool
+prop_idempotent_complement c = complement (complement c) == c
+
+prop_idempotent_revcomp    :: Base a => [a] -> Bool
+prop_idempotent_revcomp cs = reverseComplement (reverseComplement cs) == cs
