a4d1fb2bcb8447ebbd62b198500ba4fcd148848f
Vishal Sood <vishal.chandra.sood@gmail.com>
2016-11-03 13:17:38 +0100

    test clumpInts for a list, homemade way

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,7 +2,15 @@ import Dna
 
 import Test.QuickCheck
 import Test.HUnit
+import Control.Monad
 
+{- For now the tests are defined here, along with the main.
+Tests in general should fall in the same folder structure as the main library,
+locationOfTest testName = test/dir/file.hs
+where
+  dir = directoryContaining testName in src/
+  file.hs = fileContaining testName in src/dir/
+-}
 main :: IO ()
 main = do
   putStrLn " tests"
@@ -15,6 +23,10 @@ main = do
   putStrLn "test that a sequence of Base Int can be faithfully reverse complemented"
   quickCheck (prop_idempotent_revcomp :: [Int] -> Bool)
 
+  putStrLn "test that Ints are clumped correctly"
+  do
+    pss <- return $ map prop_intsCanBeClumped1 clumpEgs
+    putStrLn (foldl (\x y -> x ++ "\n" ++ y) [] pss)
 
 
 prop_idempotent_complement :: Base a => a -> Bool
@@ -22,3 +34,26 @@ prop_idempotent_complement c = complement (complement c) == c
 
 prop_idempotent_revcomp    :: Base a => [a] -> Bool
 prop_idempotent_revcomp cs = reverseComplement (reverseComplement cs) == cs
+
+clumpEgs :: [([Int], [[Int]])]
+clumpEgs = [([1,2,3,4,11] , [[11], [4,3,2,1]])
+           ,([1,2,3,4,5,12,13,14], [[14,13,12], [5,4,3,2], [4,3,2,1]])
+           ]
+prop_intsCanBeClumped :: [ ([Int], [[Int]])] -> [String]
+prop_intsCanBeClumped egs = do
+  (xs, ys) <- egs
+  return (show xs)
+  --putStrLn ((show (fst t)) ++ " --> " ++ (show (snd t)))
+  --putStrLn (if (clumpInts 3 1 (fst t) == (snd t)) then "Passed" else "Failed")
+
+prop_intsCanBeClumped1 :: ([Int], [[Int]]) -> String
+prop_intsCanBeClumped1 t = test ++ (show x) ++ sb ++ (show y) ++ resm
+  where
+    x = fst t
+    y = snd t
+    res = clumpInts 3 1 x
+    resm = if res == y
+           then " Passed"
+           else " Failed: Actual " ++ (show res)
+    sb = " should be "
+    test = "clumpInts 3 1 "
