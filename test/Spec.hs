c89b9d1427c43c3f7a557b26e196f4f97265b867
Vishal Sood <vishal.chandra.sood@gmail.com>
2016-11-11 09:33:49 +0100

    test text kmer element size

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,7 +5,7 @@ import Test.HUnit
 import Control.Monad
 import Kmers
 import qualified Data.Set as Set
-
+import qualified Data.Map as M
 {- For now the tests are defined here, along with the main.
 Tests in general should fall in the same folder structure as the main library,
 locationOfTest testName = test/dir/file.hs
@@ -18,15 +18,15 @@ main = do
   putStrLn " tests"
   putStrLn "test that Char Base can be faithfully complemented"
   quickCheck (prop_idempotent_complement :: Char -> Bool)
-  putStrLn "test that Int Base can be faithfully complemented"
+  putStrLn "that Int Base can be faithfully complemented"
   quickCheck (prop_idempotent_complement :: Int -> Bool)
-  putStrLn "test that a sequence of Base Char can be faithfully reverse complemented"
+  putStrLn "that a sequence of Base Char can be faithfully reverse complemented"
   quickCheck (prop_idempotent_revcomp :: [Char] -> Bool)
-  putStrLn "test that a sequence of Base Int can be faithfully reverse complemented"
+  putStrLn "that a sequence of Base Int can be faithfully reverse complemented"
   quickCheck (prop_idempotent_revcomp :: [Int] -> Bool)
 
   putStrLn "-------------------------------------"
-  putStrLn "test that Ints are clumped correctly"
+  putStrLn "that Ints are clumped correctly"
   do
     pss <- return $ map prop_intsCanBeClumped intClumpEgs
     putStrLn (concatStrList pss)
@@ -36,7 +36,7 @@ main = do
     putStrLn (concatStrList pss)
   putStrLn "-------------------------------------"
   do
-    pss <- return $ map prop_clumpsRegCovIsExpected clumpSizeEgs
+    pss <- return $ map prop_clumpsRegCovSize clumpSizeEgs
     putStrLn (concatStrList pss)
   where
     concatStrList :: [String] -> String
@@ -49,6 +49,14 @@ prop_idempotent_complement c = complement (complement c) == c
 
 prop_idempotent_revcomp    :: Base a => [a] -> Bool
 prop_idempotent_revcomp cs = reverseComplement (reverseComplement cs) == cs
+
+prop_textKmerSize :: (Base b, Show b) => Int -> [b] -> String
+prop_textKmerSize k text = test ++ sb ++ (show k) ++ "." ++ resm
+  where
+    test = "size of kmers obtained from  \"kmerOccs k text\""
+    resm = if all (\c -> length c == k) (M.keys $ kmerOccs k text)
+           then " Passed"
+           else " Failed"
 
 intClumpEgs :: [([Int], [[Int]])]
 intClumpEgs = [([1,2,3,4,11] , [[11], [4,3,2,1]])
@@ -81,8 +89,8 @@ prop_baseSeqCanBeClumped (x, y) = test ++ (show x) ++ sb ++ (show y) ++ resm
     test = "clumps 3 1 "
 
 
-prop_clumpsRegCovIsExpected :: (Base b, Show b) => (Clumer b, [Int]) -> String
-prop_clumpsRegCovIsExpected (x, y) = test ++ (show x) ++ sb ++ (show y) ++ resm
+prop_clumpsRegCovSize :: (Base b, Show b) => (Clumer b, [Int]) -> String
+prop_clumpsRegCovSize (x, y) = test ++ (show x) ++ sb ++ (show y) ++ resm
   where
     resm = if res == y
            then " Passed"
@@ -95,8 +103,8 @@ clumpSizeEgs = map (
   \(c, cs) -> (c, map regionCovered cs)
   ) clumerClumpEgs
 
-prop_clumpSatisfiesConstraints :: (Base b, Show b) => Int -> Int -> Int -> Clumer b -> String
-prop_clumpSatisfiesConstraints k l t c = test ++ (show c) ++ sb ++ y ++ resm
+prop_clumpParams :: (Base b, Show b) => Int -> Int -> Int -> Clumer b -> String
+prop_clumpParams k l t c = test ++ (show c) ++ sb ++ y ++ "." ++ resm
   where
     resm  = if kpass && lpass && tpass
             then " Passed"
@@ -111,5 +119,13 @@ prop_clumpSatisfiesConstraints k l t c = test ++ (show c) ++ sb ++ y ++ resm
     la    = maximum $ map regionCovered cls
     ta    = minimum $ map size cls
     cls   = clumps l t c
-    y     = "k = "(show k) ++ ", l >= " ++ (show l) ++ ", t >= " ++ (show t)
+    y     = "k = " ++ (show k) ++ ", l >= " ++ (show l) ++ ", t >= " ++ (show t)
     test  = "clump parameters of "
+
+--prop_textClumps :: (Base b, Show b) => Int -> Int -> Int -> Clumer b -> String
+--prop_textClumps k l t text = test ++ sb ++ y ++ "." ++ resm
+--where
+ -- test  = "parameters for clumps in kmers of text"
+ -- resm  = if kpass && lpass && tpass
+  --        then " Passed"
+   --       else " Failed: Actual Value " ++ ska ++ ", " ++ sla ++ ", " ++ sta
