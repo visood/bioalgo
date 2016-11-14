44ffa27ad89b4f372dd3a67f4b1868a70d453f1f
Vishal Sood <vishal.chandra.sood@gmail.com>
2016-11-14 22:58:26 +0100

    define newtype Nucleotide as Base

--- src/lib/Dna/Dna.hs
+++ src/lib/Dna/Dna.hs
@@ -1,9 +1,22 @@
-{-#LANGUAGE GADTs, StandaloneDeriving #-}
+{-#LANGUAGE GADTs, StandaloneDeriving, GeneralizedNewtypeDeriving #-}
 module Dna.Dna where
 
 import Data.List (sort)
 import Data.Set (Set)
+{-
+Ix is used to map a contiguous subrange of values in type onto integers.
+It is used primarily for array indexing (see the array package). Ix uses
+row-major order.
+-}
+import Data.Ix
+{-
+Foreign.Storable provides most elementary support for marshalling and is part
+of the language-independent portion of the Foreign Function Interface (FFI), and
+will normally be imported via the Foreign module
+-}
+import Foreign.Storable
 import qualified Data.Set as Set
+import Data.Word
 
 invalidPos :: Int
 invalidPos = -1
@@ -73,3 +86,52 @@ isDNA seq = all isBase  seq
 
 class WithBaseSeq b where
   bseq :: Base a => (b a) -> [a]
+
+newtype Nucleotide = Nuc {nuchar :: Word8} deriving (
+  Eq, Ord, Enum, Ix, Storable
+  )
+
+instance Bounded Nucleotide where
+  minBound = Nuc 0
+  maxBound = Nuc 15
+
+instance Show Nucleotide where
+  show (Nuc x) | x == 0    = "-"
+               | x == 1    = "A"
+               | x == 2    = "C"
+               | x == 4    = "G"
+               | x == 8    = "T"
+               | x == 15   = "N"
+               | otherwise = "X"
+    
+gap, _A_, _C_, _G_, _T_, _N_ :: Nucleotide
+gap = Nuc 0
+_A_ = Nuc 1
+_C_ = Nuc 2
+_G_ = Nuc 4
+_T_ = Nuc 8
+_N_ = Nuc 15
+
+instance Base Nucleotide where
+  bases = [_A_, _C_, _G_, _T_, _N_]
+  indexed 0 = gap
+  indexed 1 = _A_
+  indexed 2 = _C_
+  indexed 3 = _G_
+  indexed 4 = _T_
+  indexed _ = _N_
+  complement (Nuc 0)  = Nuc 0
+  complement (Nuc 1)  = Nuc 8
+  complement (Nuc 2)  = Nuc 4
+  complement (Nuc 4)  = Nuc 2
+  complement (Nuc 8)  = Nuc 1
+  complement (Nuc 15) = Nuc 15
+  complement x = x
+  symbol (Nuc 0)  = '-'
+  symbol (Nuc 1)  = 'A'
+  symbol (Nuc 2)  = 'C'
+  symbol (Nuc 4)  = 'G'
+  symbol (Nuc 8)  = 'T'
+  symbol (Nuc 15) = 'N'
+  symbol x        = 'X'
+
