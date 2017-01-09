1609fb4ac2802b5ef0ffe22c64b6db7cd404c2e9
Vishal Sood <vishal.chandra.sood@gmail.com>
2017-01-09 13:01:48 +0100

    develop generic lexo-sim concepts

--- src/lib/Genome/Data/FrequencyArray.hs
+++ src/lib/Genome/Data/FrequencyArray.hs
@@ -3,6 +3,7 @@ module Genome.Data.FrequencyArray where
 
 import Data.List (sort)
 import Data.Set (Set)
+import qualified Data.Set as S
 import Data.Sequence (Seq)
 import Data.Sequence ((><), (<|), (|>))
 import qualified Data.Foldable as Foldable
@@ -27,43 +28,71 @@ class Show b => Lexicord b where
     where rx = x - 1
 
 
-mostFrequentKmers :: Lexicord b => Int -> Int -> [b] -> (Int, [[b]])
-mostFrequentKmers c k text = (n, map (listlexval c)  (m ! n))
+  
+mostFreqKmers :: Lexicord b => Int -> Int -> [b] -> (Int, [[b]])
+mostFreqKmers c k text = (n, map (listlexval c)  (m ! n))
   where
-    m = freqKmerLexicords c k text
-    n = (maximum . M.keys) m    
+    m = freqKmer c k text
+    n = (maximum . M.keys) m
 
-freqKmerLexicords :: Lexicord b => Int -> Int -> [b] -> Map Int [Int]
-freqKmerLexicords c k text = invertedMap (kmerLexicordCounts c k text)
+freqKmer :: Lexicord b => Int -> Int -> [b] -> Map Int [Int]
+freqKmer c k text = invertedMap (kmerCounts c k text)
 
-kmerLexicordCounts :: Lexicord b => Int -> Int -> [b] -> Map Int Int
-kmerLexicordCounts c k text = if (length kmer) < k
-                              then M.empty
-                              else M.insertWith (+) (listlexord c kmer) 1 kcounts
+kmerCounts :: Lexicord a => Int -> Int -> [a] -> Map Int Int
+kmerCounts c k text = if (length kmer) < k
+                      then M.empty
+                      else M.insertWith (+) (listlexord c kmer) 1 kcounts
   where
     kmer = take k text
-    kcounts = kmerLexicordCounts c k (drop 1 text)
+    kcounts = kmerCounts c k (drop 1 text)
 
 
 type Simpred b = [b] -> [b] -> Bool
+type Simpats b = b -> [b]
+type Cardinality = Int
 
-mostAppxFreqKmers :: Lexicord b => Int -> Simpred b -> Int -> [b] -> (Int, [[b]])
-mostAppxFreqKmers c areSimilar k text = (n, map (listlexval c) (m ! n))
+mostSimFreqKmers :: Lexicord a => Cardinality -> Int -> [a] -> (Int, [a])
+mostSimFreqKmers c k text = (n, map (listlexval c) (m ! n))
   where
-    m = invertedMap (appxKmerLexicordCounts c areSimilar k text)
+    m = invertedMap $ (simKmerCounts c k text) :: Map Int Int
+    n = (maximum . M.keys  m)
+
+simKmerCounts :: Lexicord a, Sim b => Cardinality -> Int -> [a] -> Map b Int
+simKmerCounts c k text = foldl
+                         (\m p -> M.insert p (hcount p) m)
+                         M.empty
+                         (tpats >>= simpats)
+  where
+    kcs = kmerCounts c k text
+    hcount p = foldl (\s q -> if aresim p q then s + (kcs ! q) else s) 0 tpats
+    tpats = M.keys kcs
+
+
+-- to order lists we need the cardinality of the alphabet
+data LexicographicalOrder = Lexord Int
+class Show b => Lexicordable b where
+  lexord :: b -> Int
+  lexval :: Int -> b
+{-
+mostSimFreqKmers :: Lexicord b => Int -> Simpred b -> Simpats Int -> Int -> [b] -> (Int, [[b]])
+mostSimFreqKmers c aresim simpats k text = (n, map (listlexval c) (m ! n))
+  where
+    m = invertedMap (simKmerCounts c aresim simpats k text)
     n = (maximum . M.keys) m
 
-appxKmerLexicordCounts :: Lexicord b => Int -> Simpred b -> Int -> [b] -> Map Int Int
-appxKmerLexicordCounts c areSimilar k text = foldl
-                                             (\m p -> M.insert p (hcount p) m)
-                                             M.empty
-                                             (M.keys kcs)
+simKmerCounts :: Lexicord b => Int -> Simpred b -> Simpats Int -> Int -> [b] -> Map Int Int
+simKmerCounts c aresim simpats k text = foldl
+                                        (\m p -> M.insert p (hcount p) m)
+                                        M.empty
+                                        (tpats >>= simpats)
   where
-    kcs = kmerLexicordCounts c k text
+    kcs = kmerCounts c k text
     hcount p = foldl
                (\s q ->
-                  if areSimilar (listlexval c p) (listlexval c q)
+                  if aresim (listlexval c p) (listlexval c q)
                   then s + (kcs ! q)
                   else s)
                0
-               (M.keys kcs)
+               tpats
+    tpats = M.keys kcs
+-}
